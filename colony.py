3021b3bdcf8a45bd06da018e90229dbd8d0a8a4d
Duke Takle <duke.takle@gmail.com>
2021-03-14 19:44:54 -0500

    dekrufting

--- colony.py
+++ colony.py
@@ -27,14 +27,14 @@ class Colony:
         self.row_count = 32
         self.column_count = 64
         for idx in range(0, self.row_count):
-            currentRow = []
+            current_row = []
             for jdx in range(0, self.column_count):
                 coin = int(random.random() * 100) % seed
                 if (coin == 0):
-                    currentRow.append(True)
+                    current_row.append(True)
                 else:
-                    currentRow.append(False)
-            self.colony.append(currentRow)
+                    current_row.append(False)
+            self.colony.append(current_row)
 
         self.oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
         if os.path.isfile(SOME_FONT_FILE):
@@ -46,81 +46,39 @@ class Colony:
         self.oled.fill(0)
         self.oled.show()
 
-    def draw_four(self, x, y):
+    def display_colony(self):
         self.clear()
         image = Image.new('1', (self.oled.width, self.oled.height))
         draw = ImageDraw.Draw(image)
 
-        draw.point((x, y), fill=255)
-        draw.point((x, y+1), fill=255)
-        draw.point((x+1, y), fill=255)
-        draw.point((x+1, y+1), fill=255)
-
-        y += 2
-        draw.point((x, y), fill=255)
-        draw.point((x, y+1), fill=255)
-        draw.point((x+1, y), fill=255)
-        draw.point((x+1, y+1), fill=255)
-
-        y += 2
-        draw.point((x, y), fill=255)
-        draw.point((x, y+1), fill=255)
-        draw.point((x+1, y), fill=255)
-        draw.point((x+1, y+1), fill=255)
-
-        y -= 4
-        x += 2
-        draw.point((x, y), fill=255)
-        draw.point((x, y+1), fill=255)
-        draw.point((x+1, y), fill=255)
-        draw.point((x+1, y+1), fill=255)
-
-        x += 2
-        draw.point((x, y), fill=255)
-        draw.point((x, y+1), fill=255)
-        draw.point((x+1, y), fill=255)
-        draw.point((x+1, y+1), fill=255)
-        self.oled.image(image)
-        self.oled.show()
-
-    def displayColony(self):
-        self.clear()
-        image = Image.new('1', (self.oled.width, self.oled.height))
-        draw = ImageDraw.Draw(image)
-
-        rowNum = 0  # TODO: is the 0 right or the old 1?
+        row_num = 0  # TODO: is the 0 right or the old 1?
         for row in self.colony:
-            y = 2 * rowNum
+            y = 2 * row_num
             for idx in range(0, self.column_count):
                 if (row[idx]):
-                    # sys.stdout.write("#")
                     x = 2 * idx
                     draw.point((x, y), fill=255)
                     draw.point((x, y+1), fill=255)
                     draw.point((x+1, y), fill=255)
                     draw.point((x+1, y+1), fill=255)
-                else:
-                    # sys.stdout.write(" ")
-                    pass
-            if (rowNum < self.row_count):
-                rowNum = rowNum + 1
-                # sys.stdout.write("\n")
+            if (row_num < self.row_count):
+                row_num = row_num + 1
 
         self.oled.image(image)
         self.oled.show()
 
-    def createNextGeneration(self):
-        nextColony = []
+    def create_next_gen(self):
+        next_colony = []
 
         for idx in range(0, self.row_count):
-            currentRow = []
+            current_row = []
             for jdx in range(0, self.column_count):
-                currentRow.append(False)
-            nextColony.append(currentRow)
+                current_row.append(False)
+            next_colony.append(current_row)
 
         for y in range(0, self.row_count):
             for x in range(0, self.column_count):
-                populatedNeighborCount = 0
+                neighbor_count = 0
                 neighbors = []
                 neighbors.append((x-1, y-1))
                 neighbors.append((x, y-1))
@@ -134,21 +92,21 @@ class Colony:
                     (nx, ny) = point
                     if (nx >= 0 and nx < self.column_count and ny >= 0 and ny < self.row_count):
                         if (self.colony[ny][nx]):
-                            populatedNeighborCount = populatedNeighborCount + 1
+                            neighbor_count = neighbor_count + 1
                 if (self.colony[y][x]):
-                    if (populatedNeighborCount == 0 or populatedNeighborCount == 1):
-                        nextColony[y][x] = False
-                    elif (populatedNeighborCount == 2 or populatedNeighborCount == 3):
-                        nextColony[y][x] = True
+                    if (neighbor_count == 0 or neighbor_count == 1):
+                        next_colony[y][x] = False
+                    elif (neighbor_count == 2 or neighbor_count == 3):
+                        next_colony[y][x] = True
                     else:
-                        nextColony[y][x] = False
+                        next_colony[y][x] = False
                 else:
-                    if (populatedNeighborCount == 3):
-                        nextColony[y][x] = True
+                    if (neighbor_count == 3):
+                        next_colony[y][x] = True
                     else:
-                        nextColony[y][x] = False
+                        next_colony[y][x] = False
 
-        self.colony = nextColony
+        self.colony = next_colony
 
 
 def main():
@@ -158,12 +116,11 @@ def main():
         logger.info(wtf)
         seed = SEED
     colony = Colony(seed)
-    colony.draw_four(10, 10)
 
     generation = 0
     while (generation < MAX_GEN_COUNT):
-        colony.displayColony()
-        colony.createNextGeneration()
+        colony.display_colony()
+        colony.create_next_gen()
         generation += 1
         logger.info('generation: %s', generation)
         time.sleep(0.3)

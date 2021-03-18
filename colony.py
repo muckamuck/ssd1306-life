9bcd287be2e369fe657cb6a768dc1214820079c2
Duke Takle <duke.takle@gmail.com>
2021-03-17 22:01:30 -0500

    add some known patterns

--- colony.py
+++ colony.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import logging
 import random
@@ -17,15 +16,32 @@ BORDER = 2
 SOME_FONT_FILE = '/usr/share/fonts/truetype/liberation/LiberationMono-Bold.ttf'
 MAX_GEN_COUNT = 1000
 SEED = 5
+GENERATION_TIME = 0.5
 i2c = board.I2C()
 time.sleep(0.1)
 
 
 class Colony:
-    def __init__(self, seed):
+    def __init__(self, **kwargs):
+        seed = kwargs.get('seed', None)
+        pattern = kwargs.get('pattern', None)
+
         self.colony = []
         self.row_count = 32
         self.column_count = 64
+        if seed is not None:
+            # self.generate_random(seed)
+            # self.start_box()
+            # self.start_blinker()
+            # self.start_toad()
+            # self.start_beacon()
+            # self.start_glider()
+            # self.start_penta_decathlon()
+            self.start_pulsar()
+
+        self.oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
+
+    def generate_random(self, seed):
         for idx in range(0, self.row_count):
             current_row = []
             for jdx in range(0, self.column_count):
@@ -36,11 +52,129 @@ class Colony:
                     current_row.append(False)
             self.colony.append(current_row)
 
-        self.oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C)
-        if os.path.isfile(SOME_FONT_FILE):
-            self.use_interesting_font = True
-        else:
-            self.use_interesting_font = False
+    def start_box(self):
+        self.blank()
+        self.colony[1][1] = True
+        self.colony[1][2] = True
+        self.colony[2][1] = True
+        self.colony[2][2] = True
+
+    def start_blinker(self):
+        self.blank()
+        self.colony[1][1] = True
+        self.colony[2][1] = True
+        self.colony[3][1] = True
+
+    def start_toad(self):
+        self.blank()
+        self.colony[3][6] = True
+        self.colony[3][7] = True
+        self.colony[3][8] = True
+        self.colony[4][5] = True
+        self.colony[4][6] = True
+        self.colony[4][7] = True
+
+    def start_beacon(self):
+        self.blank()
+        self.colony[1][1] = True
+        self.colony[1][2] = True
+        self.colony[2][1] = True
+        self.colony[2][2] = True
+
+        self.colony[3][3] = True
+        self.colony[3][4] = True
+        self.colony[4][3] = True
+        self.colony[4][4] = True
+
+    def start_glider(self):
+        self.blank()
+        self.colony[0][0] = True
+        self.colony[1][1] = True
+        self.colony[1][2] = True
+        self.colony[2][0] = True
+        self.colony[2][1] = True
+
+    def start_penta_decathlon(self):
+        self.blank()
+        self.colony[4][5] = True
+        self.colony[5][5] = True
+        self.colony[6][4] = True
+        self.colony[6][6] = True
+        self.colony[7][5] = True
+        self.colony[8][5] = True
+        self.colony[9][5] = True
+        self.colony[10][5] = True
+        self.colony[11][4] = True
+        self.colony[11][6] = True
+        self.colony[12][5] = True
+        self.colony[13][5] = True
+
+    def start_pulsar(self):
+        self.blank()
+        self.colony[2][4] = True
+        self.colony[2][5] = True
+        self.colony[2][6] = True
+        self.colony[2][10] = True
+        self.colony[2][11] = True
+        self.colony[2][12] = True
+
+        self.colony[4][2] = True
+        self.colony[4][7] = True
+        self.colony[4][9] = True
+        self.colony[4][14] = True
+
+        self.colony[5][2] = True
+        self.colony[5][7] = True
+        self.colony[5][9] = True
+        self.colony[5][14] = True
+
+        self.colony[6][2] = True
+        self.colony[6][7] = True
+        self.colony[6][9] = True
+        self.colony[6][14] = True
+
+        self.colony[7][4] = True
+        self.colony[7][5] = True
+        self.colony[7][6] = True
+        self.colony[7][10] = True
+        self.colony[7][11] = True
+        self.colony[7][12] = True
+
+        self.colony[9][4] = True
+        self.colony[9][5] = True
+        self.colony[9][6] = True
+        self.colony[9][10] = True
+        self.colony[9][11] = True
+        self.colony[9][12] = True
+
+        self.colony[10][2] = True
+        self.colony[10][7] = True
+        self.colony[10][9] = True
+        self.colony[10][14] = True
+
+        self.colony[11][2] = True
+        self.colony[11][7] = True
+        self.colony[11][9] = True
+        self.colony[11][14] = True
+
+        self.colony[12][2] = True
+        self.colony[12][7] = True
+        self.colony[12][9] = True
+        self.colony[12][14] = True
+
+        self.colony[14][4] = True
+        self.colony[14][5] = True
+        self.colony[14][6] = True
+        self.colony[14][10] = True
+        self.colony[14][11] = True
+        self.colony[14][12] = True
+
+    def blank(self):
+        for _ in range(0, self.row_count):
+            current_row = []
+            for _ in range(0, self.column_count):
+                current_row.append(False)
+            self.colony.append(current_row)
 
     def clear(self):
         self.oled.fill(0)
@@ -125,7 +259,7 @@ def main():
     except Exception as wtf:
         logger.info(wtf)
         seed = SEED
-    colony = Colony(seed)
+    colony = Colony(seed=seed)
 
     generation = 0
     while (generation < MAX_GEN_COUNT):
@@ -133,7 +267,7 @@ def main():
         colony.create_next_gen()
         generation += 1
         logger.info('generation: %s', generation)
-        time.sleep(0.3)
+        time.sleep(GENERATION_TIME)
 
     time.sleep(5)
     colony.draw_image()
